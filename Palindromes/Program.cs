8b1add6662c865a858b2604d344c5d5f04516ed8
Vytautas M <Vytautas.Mikalainis@1clickfactory.com>
2015-09-26 15:36:02 +0300

    Palindromes assignment

---
+++ Palindromes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+
+            Console.WriteLine("Input polindrome word");
+            string myString = Console.ReadLine();
+            int length = myString.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (myString[i] != myString[length - i - 1])
+                     
+                Console.WriteLine("It is not polindrome");
+            }
+            Console.WriteLine("It is  polindrome");
+        }
+    }
+}
